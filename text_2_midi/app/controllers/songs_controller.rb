f631d90b457c15d1dd4bada56c143e570a913871
Gabriel Garrod <gabrielg1976@gmail.com>
2008-11-30 22:03:50 -0800

    fixed the css and removed the tutorial scaffold

--- text_2_midi/app/controllers/songs_controller.rb
+++ text_2_midi/app/controllers/songs_controller.rb
@@ -15,8 +15,7 @@ before_filter :login_required, :except => :index
   # GET /songs/1.xml
   def show
     @song = Song.find(params[:id])
-
-    respond_to do |format|
+      respond_to do |format|
       format.html # show.html.erb
       format.xml  { render :xml => @song }
     end
--- text_2_midi/config/routes.rb
+++ text_2_midi/config/routes.rb
@@ -1,6 +1,4 @@
 ActionController::Routing::Routes.draw do |map|
-  map.resources :tutorials
-
   map.resources :users, :has_many => :songs
   map.resource :session
   map.sign_up '/signup', :controller=> "users", :action=> "new"
@@ -10,7 +8,6 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :main
   map.root :controller => "main"
   map.resources :profiles,:has_many => :users
-  map.resources :tutorials 
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
 end
